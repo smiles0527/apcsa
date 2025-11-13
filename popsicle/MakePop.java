88b3a201b5c0d3be092099d37e21746343d41a60
smiles0527 <iscurt.w@gmail.com>
2025-11-12 17:45:49 -0800

    renamed lab3 to book and popsicle added popsicle lab

---
+++ popsicle/MakePop.java
@@ -0,0 +1,14 @@
+public class MakePop {
+
+    public static void main(String[] args) {
+        Popsicle treat = new Popsicle("Cherry", 100);
+
+        System.out.println("Flavour: " + treat.getFlavour());
+        System.out.println("Details: " + treat.toString());
+
+        int remaining = treat.bite(25);
+        System.out.println("After a bite, popsicle remaining: " + remaining);
+    }
+}
+
+
