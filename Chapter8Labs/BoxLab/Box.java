3ebd9773eea49ccbeeae350747660195ae914335
Curtis Wei <iscurt.w@gmail.com>
2026-02-24 20:11:41 -0800

    Updated box lab

--- Chapter8Labs/BoxLab/Box.java
+++ Chapter8Labs/BoxLab/Box.java
@@ -1,46 +1,72 @@
-public class Box{
-    private int length;
-    private int width;
-    private int height;
-    private static int number_of_boxes;
+public class Box {
 
-    public Box(){
-        this.length=0;
-        this.width=0;
-        this.height=0;
+    private double length;
+    private double width;
+    private double height;
+
+    private static int numberOfBoxes = 0;
+
+    public Box() {
+        length = 1.0;
+        width = 1.0;
+        height = 1.0;
+        numberOfBoxes++;
     }
 
-    public Box(int l, int w, int h){
-        this.length = l;
-        this.width = w;
-        this.height = h;
+    public Box(double length, double width, double height) {
+        this.length = length;
+        this.width = width;
+        this.height = height;
+        numberOfBoxes++;
     }
 
-    public Box(Box B){
+    public Box(Box b) {
         this.length = b.length;
         this.width = b.width;
         this.height = b.height;
+        numberOfBoxes++;
     }
 
-    public double getVolume() return (double)length*width*height;
-    public static void change_number_of_boxes(int in) number_of_boxes = in;
-    public void add(double x) length += x;
+    public double getVolume() {
+        return length * width * height;
+    }
 
-    public void add(double x, double y){
+    public static void change_number_of_boxes(int in) {
+        numberOfBoxes = in;
+    }
+
+    public void add(double x) {
+        length += x;
+    }
+
+    public void add(double x, double y) {
         length += x;
         width += y;
     }
 
-    public void add(double x, double y, double z){
+    public void add(double x, double y, double z) {
         length += x;
         width += y;
         height += z;
     }
 
-    public void setWidth(double w) width = w;
-    public void setLength(double l) length = l;
-    public void setHeight(double h) height = h;
-    public int getNUmberOfBoxes() return number_of_boxes;
-    public String toString(){  return ("length =" + length + " height= " + height + " width= " + width);
+    public void setWidth(double w) {
+        width = w;
+    }
 
+    public void setLength(double l) {
+        length = l;
+    }
+
+    public void setHeight(double h) {
+        height = h;
+    }
+
+    public int getNumberOfBoxes() {
+        return numberOfBoxes;
+    }
+
+    public String toString() {
+        return "Box[length=" + length + ", width=" + width + ", height=" + height + ", volume=" + getVolume() + "]";
+    }
 }
